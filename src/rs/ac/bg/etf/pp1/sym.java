7326f5d2eccec3eaf7d26a118f6ee06f577a490b
Bodin Matejic <Bodinm7@gmail.com>
2018-08-01 19:14:36 +0200

    Add sym.java file
    Defines all lexical structures

---
+++ src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,60 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+
+	// Keywords 
+	public static final int PROGRAM = 1;
+	public static final int BREAK = 2;
+	public static final int CLASS = 3;
+	public static final int ELSE = 4;
+	public static final int IF = 5;
+	public static final int NEW = 6;
+	public static final int PRINT = 7;
+	public static final int READ = 8;
+	public static final int RETURN = 9;
+	public static final int VOID = 10;
+	public static final int DO = 11;
+	public static final int WHILE = 12;
+	public static final int EXTENDS = 13;
+	public static final int CONTINUE = 14;
+	
+	// Identifiers
+	public static final int IDENT = 15;
+	
+	// Constants
+	public static final int NUM = 16;
+	public static final int CHAR = 17;
+	public static final int BOOL = 18;
+	
+	// Operators
+	public static final int ADD = 19; 		// +
+	public static final int SUB = 20; 		// -
+	public static final int MUL = 21; 		// *
+	public static final int DIV = 22; 		// /
+	public static final int MOD = 23; 		// %
+	public static final int EQ = 24;		// ==
+	public static final int NEQ = 25; 		// !=
+	public static final int GR = 26; 		// >
+	public static final int GREQ = 27; 		// >=
+	public static final int LS = 28;		// <
+	public static final int LSEQ = 29;		// <=
+	public static final int AND = 30;		// &&
+	public static final int OR = 31;		// ||
+	public static final int ASSIGN = 32;	// =
+	public static final int INC = 33;		// ++
+	public static final int DEC = 34;		// --
+	
+	public static final int SEMI = 35;		// ;
+	public static final int COMMA = 36;		// ,
+	public static final int DOT = 37;		// .
+	
+	public static final int LPAREN = 38;	// (
+	public static final int RPAREN = 39;	// )
+	public static final int LSQUARE = 40;	// [
+	public static final int RSQUARE = 41;	// ]
+	public static final int LBRACE = 42;	// {
+	public static final int RBRACE = 43;	// }
+	
+	// End of file
+	public static final int EOF = -1;
+}
