d5d6b7d3ec4f84d629a4cc33ada851c9d76f96e3
Bodin Matejic <Bodinm7@gmail.com>
2018-08-05 16:06:27 +0200

    Test: Update lexer test and example program

--- test/rs/ac/bg/etf/pp1/LexerTest.java
+++ test/rs/ac/bg/etf/pp1/LexerTest.java
@@ -5,15 +5,29 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.lang.reflect.Field;
 
 import java_cup.runtime.Symbol;
 
-public class Test {
+
+public class LexerTest {
+	
+	private static String symbolName(int id) {
+		Field[] fields = sym.class.getDeclaredFields();
+		String name = "";
+		try {
+			for (int i = 0; i < fields.length; i++)
+				if (fields[i].getInt(null) == id)
+					return fields[i].getName();
+		} 
+		catch (Exception e) { e.printStackTrace(); };
+		return null;
+	}
 
 	public static void main(String[] args) {
 		Reader br = null;
 		try {
-			File sourceCode = new File("test/program.mj");	
+			File sourceCode = new File("test/LexerProgram.mj");	
 			System.out.println("Compiling source file: " + sourceCode.getAbsolutePath());
 			
 			br = new BufferedReader(new FileReader(sourceCode));
@@ -22,7 +36,7 @@ public class Test {
 			Symbol currToken = null;
 			while ((currToken = lexer.next_token()).sym != sym.EOF) {
 				if (currToken != null && currToken.value != null)
-					System.out.println(currToken.toString() + " " + currToken.value.toString());
+					System.out.println(currToken.left + ": " + symbolName(currToken.sym) + " " + currToken.toString() + "		" + currToken.value.toString());
 			}
 		} 
 		catch (IOException e) { System.out.println(e.getMessage()); }
