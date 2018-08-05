a9f04f09a27e04b0babc74fb003c9f1144df8c67
Bodin Matejic <Bodinm7@gmail.com>
2018-08-05 03:09:49 +0200

    Rename sym to sym_old
    Cup will generate it's own sym file, so this will save the old hand written one.

---
+++ src/rs/ac/bg/etf/pp1/sym_old.java
@@ -0,0 +1,64 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym_old {
+
+	// Keywords 
+	public static final int PROGRAM = 2;
+	public static final int BREAK = 3;
+	public static final int CLASS = 4;
+	public static final int ELSE = 5;
+	public static final int IF = 6;
+	public static final int NEW = 7;
+	public static final int PRINT = 8;
+	public static final int READ = 9;
+	public static final int RETURN = 10;
+	public static final int VOID = 11;
+	public static final int DO = 12;
+	public static final int WHILE = 13;
+	public static final int EXTENDS = 14;
+	public static final int CONTINUE = 15;
+	
+	// Identifiers
+	public static final int IDENT = 16;
+	
+	// Constants
+	public static final int NUM = 17;
+	public static final int CHAR = 18;
+	public static final int BOOL = 19;
+	public static final int CONST = 20;
+	
+	// Operators
+	public static final int ADD = 21;       // +
+	public static final int SUB = 22;       // -
+	public static final int MUL = 23;       // *
+	public static final int DIV = 24;       // /
+	public static final int MOD = 25;       // %
+	public static final int EQ = 26;        // ==
+	public static final int NEQ = 27;       // !=
+	public static final int GR = 28;        // >
+	public static final int GREQ = 29;      // >=
+	public static final int LS = 30;        // <
+	public static final int LSEQ = 31;      // <=
+	public static final int AND = 32;       // &&
+	public static final int OR = 33;        // ||
+	public static final int ASSIGN = 34;    // =
+	public static final int INC = 35;       // ++
+	public static final int DEC = 36;       // --
+	
+	public static final int SEMI = 37;      // ;
+	public static final int COMMA = 38;     // ,
+	public static final int DOT = 39;       // .
+	
+	public static final int LPAREN = 40;    // (
+	public static final int RPAREN = 41;    // )
+	public static final int LSQUARE = 42;   // [
+	public static final int RSQUARE = 43;   // ]
+	public static final int LBRACE = 44;    // {
+	public static final int RBRACE = 45;    // }
+	
+	// End of file
+	public static final int EOF = 0;
+	
+	// error
+	public static final int error = 1;
+}
