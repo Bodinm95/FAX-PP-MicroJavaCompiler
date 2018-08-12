45e3448d3ec926e027ed7dbae6fd8c284a5d13a8
Bodin Matejic <Bodinm7@gmail.com>
2018-08-13 01:36:05 +0200

    Add Program visit methods

--- src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
+++ src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
@@ -21,3 +21,30 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 		System.err.println("Semantic error '" + name + "' at line:" + line + " - " + msg);
 	}
 
+// ----------------------------------------------------------------------------------------------------------------------------- //
+
+	public void visit(Program Program) {
+		TabSym.chainLocalSymbols(Program.getProgId().obj);
+		TabSym.closeScope();
+		state = Scope.GLOBAL;
+		System.out.println("Program '" + ((ProgramId)Program.getProgId()).getName() + "' successfully processed.");
+	}
+
+	public void visit(ProgramId ProgramId) {
+		ProgramId.obj = TabSym.insert(Obj.Prog, ProgramId.getName(), TabSym.noType);
+		System.out.println("Program '" + ProgramId.getName() + "' declared at line:" + ProgramId.getLine());
+		TabSym.openScope();
+		state = Scope.GLOBAL;
+	}
+
+	public void visit(Type Type) {
+		Type.struct = TabSym.noType;
+		Obj typeObj = TabSym.find(Type.getName());
+		if (typeObj == TabSym.noObj || typeObj.getKind() != Obj.Type)
+			print_error(Type.getLine(), Type.getName(), "Symbol '" + Type.getName() + "' is not a type!");
+		else
+			Type.struct = typeObj.getType();
+
+		currType = Type.struct;
+	}
+
