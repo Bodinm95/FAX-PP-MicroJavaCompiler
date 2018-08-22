d31abf8e1f829431e1cf46e13fa26890b28f9856
Bodin Matejic <Bodinm7@gmail.com>
2018-08-22 22:06:42 +0200

    Test: add CodeGenTest.java

---
+++ test/rs/ac/bg/etf/pp1/CodeGenTest.java
@@ -0,0 +1,63 @@
+package rs.ac.bg.etf.pp1;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import java_cup.runtime.Symbol;
+import rs.ac.bg.etf.pp1.ast.Program;
+import rs.ac.bg.etf.pp1.util.TabSym;
+import rs.etf.pp1.mj.runtime.Code;
+import rs.etf.pp1.symboltable.Tab;
+
+public class CodeGenTest {
+
+	public static void main(String[] args) {
+
+		Reader br = null;
+		File sourceCode = new File("test/SemanticProgram.mj");
+
+		try {
+			System.out.println("Compiling source file: " + sourceCode.getAbsolutePath());
+
+			br = new BufferedReader(new FileReader(sourceCode));
+
+			Yylex lexer = new Yylex(br);
+			MJParser parser = new MJParser(lexer);
+			Symbol sym = parser.parse();
+
+			Program program = (sym.value instanceof Program) ? (Program)sym.value : null;
+			SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer();
+
+			TabSym.init();
+
+			if (program != null)
+				program.traverseBottomUp(semanticAnalyzer);
+
+			Tab.dump();
+
+			if (parser.error > 0 || semanticAnalyzer.error > 0) {
+				System.err.println("\nParsing NOT successful: " + parser.error + " Syntax errors detected! " + semanticAnalyzer.error + " Semantic errors detected!");
+			}
+			else {
+				System.out.println("\nParsing successfully done!");
+
+				File objectFile = new File("test/CodeGenProgram.obj");
+				System.out.println("\nGenerating object file: test/CodeGenProgram.obj");
+
+				CodeGenerator codegen = new CodeGenerator();
+				program.traverseBottomUp(codegen);
+
+				Code.write(new FileOutputStream(objectFile));
+			}
+		}
+		catch (Exception e) { e.printStackTrace(); }
+		finally { 
+			if (br != null) 
+				try { br.close(); } catch (IOException e) { System.out.println(e.getMessage()); }
+		}
+	}
+}
