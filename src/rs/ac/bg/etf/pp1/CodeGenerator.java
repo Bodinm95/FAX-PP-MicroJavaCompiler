66fda5db71c877041db068db6938b79a117ff69f
Bodin Matejic <Bodinm7@gmail.com>
2018-08-23 14:51:00 +0200

    Add Factor visit methods

--- src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -1,7 +1,54 @@
 package rs.ac.bg.etf.pp1;
 
 import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
+import rs.ac.bg.etf.pp1.util.TabSym;
+import rs.etf.pp1.mj.runtime.Code;
+import rs.ac.bg.etf.pp1.ast.*;
 
 public class CodeGenerator extends VisitorAdaptor {
 
+// ----------------------------------------------------------- Factor ----------------------------------------------------------- //
+
+	public void visit(FactorDesignator FactorDesignator)
+	{
+		Code.load(FactorDesignator.getDesignator().obj);
+	}
+
+	public void visit(FactorProcCall FactorProcCall)
+	{
+		// TO DO
+	}
+
+	public void visit(FactorNum FactorNum)
+	{
+		Code.loadConst(FactorNum.getValue());
+	}
+
+	public void visit(FactorChar FactorChar)
+	{
+		Code.loadConst(FactorChar.getValue());
+	}
+
+	public void visit(FactorBool FactorBool)
+	{
+		if(FactorBool.getValue().equals("1"))
+			Code.loadConst(1);
+		else
+			Code.loadConst(0);
+	}
+
+	public void visit(FactorNew FactorNew)
+	{
+		Code.put(Code.new_);
+		Code.put2(FactorNew.struct.getNumberOfFields() * 4);
+	}
+
+	public void visit(FactorNewArray FactorNewArray)
+	{
+		Code.put(Code.newarray);
+		if (FactorNewArray.getType().struct.equals(TabSym.charType))
+			Code.put(0);
+		else
+			Code.put(1);
+	}
 }
